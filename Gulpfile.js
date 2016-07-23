5829827e0be5f104665431c268123b95e0f3f79a
Yin Tao <yintao@parim.net>
2016-07-23 08:56:40 +0800

    Add dist task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var gulpLoadPugins = require('gulp-load-plugins');
 var browserSync = require('browser-sync');
+const del = require('del');
 
 const $p = gulpLoadPugins();
 const reload = browserSync.reload;
@@ -8,7 +9,8 @@ const reload = browserSync.reload;
 gulp.task('styles', function () {
     return gulp.src('src/styles/*.scss')
         .pipe($p.sass.sync().on('error', $p.sass.logError))
-        .pipe(gulp.dest('.tmp/styles'));
+        .pipe(gulp.dest('.tmp/styles'))
+        .pipe(gulp.dest('dist/styles'));
 });
 
 gulp.task('templates', function () {
@@ -33,7 +35,8 @@ gulp.task('templates', function () {
 gulp.task('scripts', ['templates'], function () {
     return gulp.src(['src/scripts/**/*.js', '.tmp/templates/**/*.js'])
         .pipe($p.concat('main.js'))
-        .pipe(gulp.dest(".tmp/scripts/"));
+        .pipe(gulp.dest(".tmp/scripts"))
+        .pipe(gulp.dest('dist/scripts'));
 });
 
 gulp.task('serve:demo', ['styles', 'scripts'], function () {
@@ -57,4 +60,13 @@ gulp.task('serve:demo', ['styles', 'scripts'], function () {
     gulp.watch('src/styles/**/*.scss', ['styles']);
     gulp.watch('src/scripts/**/*.js', ['scripts']);
     gulp.watch('src/templates/**/*.html', ['scripts']);
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('dist', ['styles', 'scripts'], function () {
+});
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('dist');
 });
---
+++ dist/scripts/main.js
@@ -0,0 +1,150 @@
+/**
+ * Created by tony on 16-6-12.
+ */
+
+/**
+ * Created by tony on 16-6-13.
+ */
+(function () {
+    'use strict';
+
+    angular.module('qui.viewFramework.sidebar', [])
+        .controller('QuiSidebarController', SidebarController)
+        .directive('quiSidebar', QuiSidebar);
+
+    function SidebarController($scope) {
+
+        $scope.isSidebarFold = $scope.config.sidebar == 'full';
+
+        return {
+            toggleFold: toggleFold
+        };
+
+        function toggleFold(){
+            if($scope.isSidebarFold){
+                $scope.config.sidebar = 'mini';
+            }else{
+                $scope.config.sidebar = 'full';
+            }
+            $scope.isSidebarFold = !$scope.isSidebarFold;
+        }
+    }
+    SidebarController.$injecte = ['$scope'];
+
+    function QuiSidebar() {
+        return {
+            controller: 'QuiSidebarController',
+            controllerAs: 'quiSidebarCtrl',
+            templateUrl: function (element, attrs) {
+                return attrs.templateUrl || 'qui/templates/sidebar/sidebar.html';
+            },
+            transclude: true,
+            replace: true
+        }
+    }
+})();
+/**
+ * Created by tony on 16-6-13.
+ */
+(function () {
+    'use strict';
+
+    angular.module('qui.viewFramework.topbar', [])
+        .controller('QuiTopbarController', TopbarController)
+        .directive('quiTopbar', QuiTopbar);
+
+    function TopbarController() {
+    }
+
+    function QuiTopbar() {
+        return {
+            templateUrl: function (element, attrs) {
+                return attrs.templateUrl || 'qui/templates/topbar/topbar.html';
+            },
+            transclude: true,
+            replace: true
+        }
+    }
+})();
+/**
+ * Created by tony on 16-6-13.
+ */
+/**
+ * Created by tony on 16-6-13.
+ */
+(function(){
+    'use strict';
+
+    angular.module('qui.viewFramework', ['qui.viewFramework.topbar', 'qui.viewFramework.sidebar'])
+        .controller('QuiViewFrameworkController', ViewFrameworkController)
+        .directive('quiViewFramework', QuiViewFramework);
+
+    function ViewFrameworkController($scope){
+        $scope.config = {
+            disableNavigation: false,
+            hideSidebar: false,
+            hideTopbar: false,
+            sidebar: 'full' //String:['full', 'mini']
+        }
+    }
+    ViewFrameworkController.$inject = ['$scope'];
+
+    function QuiViewFramework(){
+        return {
+            controller: 'QuiViewFrameworkController',
+            controllerAs: 'viewFramework',
+            templateUrl: function(element, attrs){
+                return attrs.templateUrl || 'qui/templates/view-framework/view-framework.html';
+            },
+            transclude: true,
+            replace: true,
+            scope: {
+            }
+        };
+    }
+})();
+(function(module) {
+try {
+  module = angular.module('qui.viewFramework.sidebar');
+} catch (e) {
+  module = angular.module('qui.viewFramework.sidebar', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('qui/templates/sidebar/sidebar.html',
+    '<div class="viewFramework-sidebar">\n' +
+    '    <div class="sidebar-fold" ng-click="quiSidebarCtrl.toggleFold()"><i class="fa fa-reorder" ng-class="{\'fa-rotate-90\': isSidebarFold}"></i></div>\n' +
+    '</div>');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('qui.viewFramework.topbar');
+} catch (e) {
+  module = angular.module('qui.viewFramework.topbar', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('qui/templates/topbar/topbar.html',
+    '<div class="viewFramework-topbar">\n' +
+    '\n' +
+    '</div>');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('qui.viewFramework');
+} catch (e) {
+  module = angular.module('qui.viewFramework', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('qui/templates/view-framework/view-framework.html',
+    '<div class="viewFramework">\n' +
+    '    <qui-topbar></qui-topbar>\n' +
+    '    <div class="viewFramework-body"\n' +
+    '         ng-class="{\'viewFramework-sidebar-mini\':(!config.disableNavigation && !config.hideSidebar) && config.sidebar == \'mini\',\'viewFramework-sidebar-full\': (!config.disableNavigation && !config.hideSidebar) && config.sidebar == \'full\',\'viewFramework-topbar-hide\':config.disableNavigation || config.hideTopbar}">\n' +
+    '        <qui-sidebar></qui-sidebar>\n' +
+    '    </div>\n' +
+    '</div>');
+}]);
+})();
